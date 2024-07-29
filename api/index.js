b19b2b89ca58191a25b570aae54405955e3b142c
Ugochukwu Augustine <austinjasper07@gmail.com>
2024-07-29 16:12:38 +0100

    Modified the index.js for readability

--- api/index.js
+++ api/index.js
@@ -1,13 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from "./routes/UserRoute.js";
+import signUp from './routes/Sign-up.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Middleware to parse JSON and URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.urlencoded({extended: false}));
+// Route Handlers
+app.use('/user', userRouter);
+app.use('/sign-up', signUp);
 
+// MongoDB Connection
+mongoose.connect("mongodb://127.0.0.1:27017/real-estate")
+  .then(() => { console.log("Database Connected successfully"); })
+  .catch((err) => console.log(err));
 
-
-app.listen(3000, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
